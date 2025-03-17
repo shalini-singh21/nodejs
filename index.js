b9bc1d82d4c7290a1f490316482d9f1608502cc6
shalini singh <shalu.singh13042gmail.com>
2025-03-17 16:49:35 +0530

    removing extra comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//creating server
-// const http = require('http');
-// http.createServer((Request, Response) => {
-//     Response.writeHead(200, { "content-type": 'application\json' }); //to define status code 
-//     Response.write(JSON.stringify({ name: 'shalini singh', email: "shalu.1304@gmail.com" }));
-//     Response.end();//signals the end of the response 
-// }).listen(4000); //used to start http server 
+creating server
+const http = require('http');
+http.createServer((Request, Response) => {
+    Response.writeHead(200, { "content-type": 'application\json' }); //to define status code 
+    Response.write(JSON.stringify({ name: 'shalini singh', email: "shalu.1304@gmail.com" }));
+    Response.end();//signals the end of the response 
+}).listen(4000); //used to start http server 
 
 // Handle Asynchronous Data
 console.log("statement 1");
