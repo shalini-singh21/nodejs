db28383fcccab95cf01abcd83d9c6b46460e9fb0
shalini singh <shalu.singh13042gmail.com>
2025-03-17 15:28:06 +0530

    adding nodejs file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hiiii")
+const http = require('http');
+http.createServer((Request, Response) => {
+    Response.writeHead(200, { "content-type": 'application\json' }); //to define status code 
+    Response.write(JSON.stringify({ name: 'shalini singh', email: "shalu.1304@gmail.com" }));
+    Response.end();//signals the end of the response 
+}).listen(4000); //used to start http server 
